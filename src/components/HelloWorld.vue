121a4b4781c007e58511c85d14080b799150a47b
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-09-24 22:07:05 -0400

    small edits

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,79 @@
 <template>
   <div class="q-pa-md row justify-center align-center full-height full-width text-center back">
-    <q-card class="my-card text-white">
-      <q-card-section class="cardsection">
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-      <q-separator dark class='relative-bottom'/>
-      <q-card-actions class='vertical-bottom'>
-        <q-btn flat @click="testr">Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
-      </q-card-actions>
-    </q-card>
+    <div class="q-pa-md justify-center">
+    <q-btn label="Reset" push color="white" text-color="primary" @click="step = 1" class="q-mb-md" />
+
+    <q-stepper
+      v-model="step"
+      header-nav
+      ref="stepper"
+      color="primary"
+      animated
+    >
+      <q-step
+        :name="1"
+        title="Customize"
+        icon="settings"
+        :done="step > 1"
+        :header-nav="step > 1"
+      >
+      <q-input filled v-model="text" label="Filled" />
+      <q-input
+        v-model="text"
+        filled
+        type="textarea"
+      />
+
+        <q-stepper-navigation>
+          <q-btn @click="() => { done1 = true; step = 2 }" color="primary" label="Continue" />
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step
+        :name="2"
+        title="Fund"
+        icon="create_new_folder"
+        :done="step > 2"
+        :header-nav="step > 2"
+      >
+        An ad group contains one or more ads which target a shared set of keywords.
+
+        <q-stepper-navigation>
+          <q-btn @click="() => { done2 = true; step = 3 }" color="primary" label="Continue" />
+          <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step
+        :name="3"
+        title="Confirm"
+        icon="add_comment"
+        :header-nav="step > 3"
+      >
+        Try out different ad text to see what brings in the most customers, and learn how to
+        enhance your ads using features like ad extensions. If you run into any problems with
+        your ads, find out how to tell if they're running and how to resolve approval issues.
+
+        <q-stepper-navigation>
+          <q-btn color="primary" @click="done3 = true" label="Finish" />
+          <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+    </q-stepper>
+  </div>
   </div>
 </template>
 <script>
-import test from '../assets/test.js'
+// import test from '../assets/test.js'
+import { ref } from 'vue'
 export default {
   setup () {
-  },
-  methods: {
-    async testr() {
-      console.log('hip')
-      const yes = await test()
-      console.log(yes)
+    let step = ref(1)
+    let text = ref('')
+    return {
+      step, text
     }
-  },
+  }
 }
 </script>
 
