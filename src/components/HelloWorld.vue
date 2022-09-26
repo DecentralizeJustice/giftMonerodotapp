19a585e6e7432d511ec585d3859be44e86c7ea23
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-09-25 23:03:29 -0400

    Refine ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,17 +12,25 @@
     >
       <q-step
         :name="1"
-        title="Customize"
+        title="Custamize"
         icon="settings"
         :done="step > 1"
         :header-nav="step > 1"
       >
-      <q-input filled v-model="text" label="Filled" />
-      <q-input
-        v-model="text"
-        filled
-        type="textarea"
-      />
+      <div class="q-pa-md">
+        <q-select rounded standout v-model="model" :options="options" label="Select Theme" />
+      </div>
+      <div class="q-pa-md">
+      <q-input filled v-model="text" label="To:" />
+      </div>
+      <div class="q-pa-md">
+        <q-input
+          label="Message"
+          v-model="message"
+          filled
+          type="textarea"
+        />
+      </div>
 
         <q-stepper-navigation>
           <q-btn @click="() => { done1 = true; step = 2 }" color="primary" label="Continue" />
@@ -31,12 +39,12 @@
 
       <q-step
         :name="2"
-        title="Fund"
+        title="Refund Address"
         icon="create_new_folder"
         :done="step > 2"
         :header-nav="step > 2"
       >
-        An ad group contains one or more ads which target a shared set of keywords.
+
 
         <q-stepper-navigation>
           <q-btn @click="() => { done2 = true; step = 3 }" color="primary" label="Continue" />
@@ -46,9 +54,25 @@
 
       <q-step
         :name="3"
-        title="Confirm"
+        title="Fund"
         icon="add_comment"
         :header-nav="step > 3"
+        :done="step > 3"
+      >
+        Try out different ad text to see what brings in the most customers, and learn how to
+        enhance your ads using features like ad extensions. If you run into any problems with
+        your ads, find out how to tell if they're running and how to resolve approval issues.
+
+        <q-stepper-navigation>
+          <q-btn @click="() => { done3 = true; step = 4 }" color="primary" label="Continue" />
+          <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+      <q-step
+        :name="4"
+        title="Share"
+        icon="add_comment"
+        :header-nav="step > 4"
       >
         Try out different ad text to see what brings in the most customers, and learn how to
         enhance your ads using features like ad extensions. If you run into any problems with
@@ -70,8 +94,11 @@ export default {
   setup () {
     let step = ref(1)
     let text = ref('')
+    let message = ref('')
+    let options = ['Birthday', 'Halloween', 'Secret']
+    let model = ref(null)
     return {
-      step, text
+      step, text, options, model, message
     }
   }
 }
