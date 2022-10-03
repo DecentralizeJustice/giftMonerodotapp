edf7c872f930394de89d94ae393016d6436768d2
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-10-03 13:47:39 -0400

    Refund Address page functioning

--- src/components/refund/refundAddress.vue
+++ src/components/refund/refundAddress.vue
@@ -1,37 +1,52 @@
 <template>
-  <div class="q-pa-md">
+  <div class="row justify-center">
     <div
-      class="q-gutter-md"
-      style="max-width: 300px"
+      class="col-md-8 col-12"
+      :class="{'q-pa-sm q-mt-lg': $q.screen.lt.md,
+      'q-pa-sm q-mt-md': $q.screen.gt.sm}"
     >
       <q-input
-        v-model="text"
+        v-model="refundAddress"
         label="Refund Address"
+        filled
+      type="textarea"
       />
     </div>
+
     <q-card
-      class="text-white q-ma-sm flat"
+      class="text-white q-ma-sm"
+      style="border-radius: 10px;"
+    >
+      <q-card-section
+        class="bg-black"
+      >
+        <div class="text-subtitle1 text-left">
+          Enter the address you would like to have the monero refunded to in case the gift recipeint does not access it in time.
+        </div>
+      </q-card-section>
+    </q-card>
+    <q-card
+      class="text-white q-ma-sm flat col col-md-5 col-10"
       style="border-radius: 10px;"
     >
       <q-card-section
         class="bg-grey-8"
       >
-        <div class="text-subtitle2">
-          Secret Password: Don't Share!!!
+        <div class="text-subtitle2 text-center">
+          Here are some donation options:
         </div>
+        <div class="q-pa-sm q-gutter-sm">
+           <q-btn color="primary" label="Rhino Fuacet" @click="donateToRhinoStagenet()" />
+         </div>
       </q-card-section>
     </q-card>
-  </div>
+      </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue'
-
-export default {
-  setup () {
-    return {
-      refundAddress: ref('')
-    }
-  }
+const refundAddress = ref('')
+function donateToRhinoStagenet () {
+  refundAddress.value = '73a4nWuvkYoYoksGurDjKZQcZkmaxLaKbbeiKzHnMmqKivrCzq5Q2JtJG1UZNZFqLPbQ3MiXCk2Q5bdwdUNSr7X9QrPubkn'
 }
 </script>
