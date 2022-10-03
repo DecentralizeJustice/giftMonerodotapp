4723d66a0623e7c5adb089bf8a1ce3d77291cd01
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-10-02 21:43:03 -0400

    General refund work

--- src/components/refund/refundAddress.vue
+++ src/components/refund/refundAddress.vue
@@ -6,81 +6,21 @@
     >
       <q-input
         v-model="text"
-        label="Standard"
-      />
-
-      <q-input
-        v-model="text"
-        filled
-        label="Filled"
-      />
-
-      <q-input
-        v-model="text"
-        outlined
-        label="Outlined"
-      />
-
-      <q-input
-        v-model="text"
-        standout
-        label="Standout"
-      />
-
-      <q-input
-        v-model="text"
-        standout="bg-teal text-white"
-        label="Custom standout"
-      />
-
-      <q-input
-        v-model="text"
-        borderless
-        label="Borderless"
-      />
-
-      <q-input
-        v-model="text"
-        rounded
-        filled
-        label="Rounded filled"
-      />
-
-      <q-input
-        v-model="text"
-        rounded
-        outlined
-        label="Rounded outlined"
-      />
-
-      <q-input
-        v-model="text"
-        rounded
-        standout
-        label="Rounded standout"
-      />
-
-      <q-input
-        v-model="text"
-        square
-        filled
-        label="Square filled"
-      />
-
-      <q-input
-        v-model="text"
-        square
-        outlined
-        label="Square outlined"
-      />
-
-      <q-input
-        v-model="text"
-        square
-        standout
-        label="Square standout"
+        label="Refund Address"
       />
     </div>
+    <q-card
+      class="text-white q-ma-sm flat"
+      style="border-radius: 10px;"
+    >
+      <q-card-section
+        class="bg-grey-8"
+      >
+        <div class="text-subtitle2">
+          Secret Password: Don't Share!!!
+        </div>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
@@ -90,7 +30,7 @@ import { ref } from 'vue'
 export default {
   setup () {
     return {
-      text: ref('')
+      refundAddress: ref('')
     }
   }
 }
