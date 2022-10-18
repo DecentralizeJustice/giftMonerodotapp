413093c3468e257303836bb051cf51cdc474928e
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-10-18 10:47:19 -0400

    Secure Random Number

--- src/components/create/customize/customizeCard.vue
+++ src/components/create/customize/customizeCard.vue
@@ -2,6 +2,7 @@
   <div
     class="justify-center full-height full-width text-center row"
     :class="{'justify-around': $q.screen.gt.sm}"
+    v-if="dataReady"
   >
     <div class="column col-12 col-md-6">
       <displayCardInfo :cardinfoobject="cardinfoobject" />
@@ -51,7 +52,9 @@
 </template>
 <script setup>
 import displayCardInfo from '@/components/create/customize/displayCardInfo.vue'
-import { reactive, onUpdated, defineEmits, onMounted, onActivated, defineProps, toRef, watch } from 'vue'
+import { reactive, onUpdated, defineEmits, onMounted, onActivated, defineProps, toRef, watch, ref } from 'vue'
+const _sodium = require('libsodium-wrappers')
+const dataReady = ref(false)
 const emit = defineEmits(['update-card'])
 const options = ['People', 'Animals', 'Halloween', 'Christmas']
 const clear = function () {
@@ -82,12 +85,6 @@ let cardinfoobject = {
   createdAt: 0
 }
 cardinfoobject = reactive(cardinfoobject)
-function getRandomInt (min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  // The maximum is exclusive and the minimum is inclusive
-  return Math.floor(Math.random() * (max - min) + min)
-}
 function updateCardInfo () {
   emit('update-card', cardinfoobject)
 }
@@ -100,14 +97,21 @@ onUpdated(() => {
 onActivated(() => {
   genRandomData()
 })
-function genRandomData () {
+async function genRandomData () {
+  dataReady.value = true
   cardinfoobject.entropyData.avatars[0] = 0 // constant first avatar to encode template type
   for (let i = 0; i < 8; i++) {
-    cardinfoobject.entropyData.words[i] = getRandomInt(0, 65536) // 2^16
+    cardinfoobject.entropyData.words[i] = await getRandomInt(65536) // 2^16
   }
   for (let i = 1; i < 4; i++) {
-    cardinfoobject.entropyData.avatars[i] = getRandomInt(0, 19)
+    cardinfoobject.entropyData.avatars[i] = await getRandomInt(20)
   }
+}
+async function getRandomInt (max) {
+  await _sodium.ready
+  const sodium = _sodium
+  // The min is 0 (inclusive) and the max is exclusive
+  return sodium.randombytes_uniform(max)
 }
 </script>
 
