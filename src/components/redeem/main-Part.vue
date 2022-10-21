24627dcea28e104fcc1614f800e76e9a45b0234f
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-10-20 21:08:52 -0400

    Working Card Entering

--- src/components/redeem/main-Part.vue
+++ src/components/redeem/main-Part.vue
@@ -28,20 +28,18 @@
               class="col themeCard q-ma-sm q-pa-sm"
               @click="selectTheme(index)"
             >
-              <div class="no-margin no-padding">
-                <img
-                  :src="theme[0]"
-                  class="no-margin no-padding theIcon"
-                >
-              </div>
+              <getSVG
+                :cardinfoobject="reactive({ theme: index })"
+                :chosenavatarindex="0"
+              />
             </div>
           </div>
         </q-card-section>
         <q-card-section
-          v-if="selectedTheme !== ''"
+          v-if="selectedTheme !== '' && avatarOrWordSelector === 'words'"
           class="q-pt-sm text-black text-h6"
         >
-          Fill In Your First Panel
+          Fill In Your {{ panelIteration }} Panel Words
           <div class="row q-col-gutter-sm justify-center align-center q-mt-md">
             <q-card
               class="text-white text-center q-pb-sm col-4"
@@ -51,36 +49,54 @@
                 style="margin-bottom: -5%;"
                 class="text-left text-h5 q-pt-sm q-pl-sm"
               >
-                {{ 1 }}.
+                {{ currentIncompleteCardIndex + 1 }}.
               </div>
               <q-card-section class="no-margin no-padding">
                 <div class="no-margin no-padding">
-                  <img
-                    v-for="(item, index) in themeDict[selectedTheme]"
-                    :key="index"
-                    :src="themeDict[selectedTheme][index]"
-                    class="no-margin no-padding"
-                    :class="{'theIcon': isThisTheIcon(index), 'notTheIcon': isNotTheIcon(index)}"
-                  >
+                  <getSVG
+                    :cardinfoobject="reactive({ theme: selectedTheme })"
+                    :chosenavatarindex="avatars[currentIncompleteCardIndex]"
+                  />
                 </div>
               </q-card-section>
               <q-card-section
                 class="text-subtitle1 text-weight-regular text-center text-black"
                 style=""
               >
+                <q-chip
+                  v-if="!text1Valid"
+                  icon="search"
+                  :ripple="false"
+                  color="red-10"
+                  dark
+                >
+                  Not Valid Word, Check Again
+                </q-chip>
                 <q-input
                   v-model="text1"
                   outlined
                   color="black"
                   bg-color="grey-3"
                   label="First Word"
+                  :error="!text1Valid"
                 /> <br>
+                <q-chip
+                  v-if="!text2Valid"
+                  icon="search"
+                  :ripple="false"
+                  color="red-10"
+                  dark
+                >
+                  Not Valid Word, Check Again
+                </q-chip>
                 <q-input
                   v-model="text2"
                   outlined
                   color="black"
                   bg-color="grey-3"
                   label="Second Word"
+                  :error="!text2Valid"
+                  hide-bottom-space
                 />
                 <q-btn
                   color="green"
@@ -92,44 +108,141 @@
             </q-card>
           </div>
         </q-card-section>
+        <q-card-section
+          v-if="selectedTheme !== '' && avatarOrWordSelector === 'avatar'"
+          class="q-pt-sm text-black text-h6"
+        >
+          Select Your {{ panelIteration }} Panel Avatar
+          <div class="row q-col-gutter-sm justify-center align-center q-mt-md">
+            <div
+              v-for="(svg, index) in themeDict[selectedTheme]"
+              :key="svg"
+              class="col-1"
+              @click="selectAvatar(index)"
+            >
+              <img
+                :src="svg"
+                class="no-margin no-padding iconGrid"
+              >
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-section
+          v-if="words[0] !== null"
+          class="row q-col-gutter-sm justify-center align-center q-pb-md"
+        >
+          <div
+            v-for=" (element, index) in completedCardsInfo"
+            :key="index"
+            class="text-white text-center q-pb-sm q-ma-sm col col-4 col-md-2"
+            style="background: #ff6600;  border-radius: 25px;"
+          >
+            <div
+              style="margin-bottom: -5%;"
+              class="text-left text-h5 q-pt-sm q-pl-sm"
+            >
+              {{ index + 1 }}.
+            </div>
+            <div class="no-margin no-padding">
+              <getSVG
+                :cardinfoobject="reactive({ theme: selectedTheme })"
+                :chosenavatarindex="element.avatar"
+              />
+            </div>
+            <div
+              class="text-subtitle1 text-weight-regular text-center no-padding"
+              style="line-height: 115%;"
+            >
+              {{ wordList[element.words[0]] }} <br>
+              {{ wordList[element.words[1]] }}
+            </div>
+          </div>
+        </q-card-section>
       </q-card>
     </div>
   </div>
 </template>
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, computed, reactive } from 'vue'
 import halloweenSVGList from '@/assets/svgs/halloweenSVGList.js'
 import funnyPeopleSVGList from '@/assets/svgs/funnyPeopleSVGList.js'
 import animalsSVGList from '@/assets/svgs/animalsSVGList.js'
 import christmasSVGList from '@/assets/svgs/christmasSVGList.js'
 import text from '@/assets/word-list-65555.txt'
+import getSVG from '@/components/create/customize/getSVG.vue'
 import { getWordListArray } from '@/assets/misc.js'
-const avatarIndex = 0
 const selectedTheme = ref('')
 const text1 = ref('')
 const text2 = ref('')
-function isThisTheIcon (index) {
-  if (avatarIndex === index) {
-    return true
-  }
-  return false
+const words = ref([null, null, null, null, null, null, null, null])
+const avatars = ref([0, null, null, null])
+const text1Valid = ref(true)
+const text2Valid = ref(true)
+watch(text1, () => {
+  text1Valid.value = true
+})
+watch(text2, () => {
+  text2Valid.value = true
+})
+function selectTheme (theme) {
+  selectedTheme.value = theme
 }
-function isNotTheIcon (index) {
-  if (avatarIndex === index) {
-    return false
-  }
-  return true
-}
-function selectTheme (params) {
-  selectedTheme.value = params
+function selectAvatar (avatarIndex) {
+  avatars.value[currentIncompleteCardIndex.value] = avatarIndex
 }
 const themeDict = { People: funnyPeopleSVGList, Animals: animalsSVGList, Halloween: halloweenSVGList, Christmas: christmasSVGList }
 const wordList = getWordListArray(text)
 function checkInputs () {
   const firstIndex = findWordListIndex(text1.value)
   const secondIndex = findWordListIndex(text2.value)
-  console.log(firstIndex, secondIndex)
+  text1Valid.value = firstIndex
+  text2Valid.value = secondIndex
+  if (firstIndex && secondIndex) {
+    const value = currentIncompleteWordsIndex.value
+    words.value[value] = firstIndex
+    words.value[value + 1] = secondIndex
+    text1.value = ''
+    text2.value = ''
+  }
 }
+const currentIncompleteCardIndex = computed(() => {
+  for (let index = 0; index < words.value.length; index += 2) {
+    const element = words.value[index + 1]
+    if (element === null) { return index / 2 }
+  }
+  return 3
+})
+const completedCardsInfo = computed(() => {
+  const completedCards = []
+  for (let index = 0; index < words.value.length; index += 2) {
+    const element = avatars.value[index / 2]
+    if (words.value[index + 1] !== null) { completedCards.push({ avatar: element, words: [words.value[index], words.value[index + 1]] }) }
+  }
+  console.log(completedCards)
+  return completedCards
+})
+const currentIncompleteWordsIndex = computed(() => {
+  for (let index = 0; index < words.value.length; index += 2) {
+    const element = words.value[index + 1]
+    if (element === null) { return index }
+  }
+  return 6
+})
+const panelIteration = computed(() => {
+  if (currentIncompleteCardIndex.value === 0) { return 'First' }
+  if (currentIncompleteCardIndex.value === 1) { return 'Second' }
+  if (currentIncompleteCardIndex.value === 2) { return 'Third' }
+  return 'Fourth'
+})
+const avatarOrWordSelector = computed(() => {
+  for (let index = 0; index < words.value.length; index += 2) {
+    const currentAvatar = avatars.value[index / 2]
+    const currentWords = words.value[index]
+    if (currentAvatar === null) { return 'avatar' }
+    if (currentWords === null) { return 'words' }
+  }
+  return 'done'
+})
 function findWordListIndex (word) {
   for (let index = 0; index < wordList.length; index++) {
     const potentialWord = wordList[index]
@@ -142,16 +255,12 @@ function findWordListIndex (word) {
 </script>
 
 <style lang="sass" scoped>
-
-.theIcon
-  height: 70%
-  width: 70%
-.notTheIcon
-  height: .001%
-  width: .001%
 .localCardHeader
   background-color: $secondary
 .themeCard
   background-color: $primary
   border-radius: 25px
+.iconGrid
+  height: 100%
+  width: 100%
 </style>
