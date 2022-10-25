6801e40575e260659294b76c544ad044671f6b72
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2022-10-25 18:01:03 -0400

    Node down, hold work

--- src/components/redeem/main-Part.vue
+++ src/components/redeem/main-Part.vue
@@ -10,19 +10,25 @@
     :theme="theme"
     @gotCardData="gotCardData"
   />
+  <redeemActionPanel
+    v-if="cardData !== {} && theme !== ''"
+    :cardData1="cardData"
+  />
 </template>
 <script setup>
 import enterCardInfo from '@/components/redeem/enterCardInfo.vue'
 import suspenseHolder from '@/components/redeem/suspenseHolder.vue'
+import redeemActionPanel from '@/components/redeem/redeemActionPanel.vue'
 import { ref } from 'vue'
 const entropy = ref({})
 const theme = ref('')
+const cardData = ref({})
 function caredEntered (entropyParam, themeParam) {
   entropy.value = entropyParam
   theme.value = themeParam
 }
-function gotCardData (paramst) {
-  console.log(paramst)
+function gotCardData (params) {
+  cardData.value = params
 }
 </script>
 
---
+++ src/components/redeem/redeemActionPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-pa-md row justify center q-gutter-md">
+    <q-card class="my-card col c-6">
+      <q-card-section>
+        Checking Wallet Balance
+      </q-card-section>
+    </q-card>
+  </div>
+  <q-dialog v-model="checkingForBalance">
+    <checkBalance :single-card-info="cardData" />
+  </q-dialog>
+</template>
+
+<script setup>
+import { toRef, ref } from 'vue'
+import checkBalance from '@/components/misc/checkBalance.vue'
+const props = defineProps({
+  // theme: { type: String, required: true },
+  cardData1: { type: Object, required: true }
+})
+const checkingForBalance = ref(true)
+const cardData = toRef(props, 'cardData1')
+console.log(cardData)
+</script>
